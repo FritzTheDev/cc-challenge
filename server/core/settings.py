9f04d355a6c3a9d1cfe1b9ebde237067b38ead99
Frederick Johnson <fritzj96@gmail.com>
2022-02-24 10:48:40 -0600

    added: rest_framework & api apps to settings

--- server/core/settings.py
+++ server/core/settings.py
@@ -32,12 +32,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party Apps
+    'rest_framework',
+
+    # Project Apps
+    'api'
 ]
 
 MIDDLEWARE = [
