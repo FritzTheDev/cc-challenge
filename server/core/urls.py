e9fb03bce14e6a89de28c67aecf80fc6488730ff
Frederick Johnson <fritzj96@gmail.com>
2022-02-24 11:21:02 -0600

    configured an external urlconf

--- server/core/urls.py
+++ server/core/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from api import urls
 from django.contrib import admin
-from django.urls import path
-from api import views
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', views.HelloWorldView.as_view(), name="hello")
+    path("admin/", admin.site.urls),
+    path("", include(urls)),
+    path("token/", obtain_auth_token, name="api_token_auth"),
 ]
