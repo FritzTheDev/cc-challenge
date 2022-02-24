bec990e165f69edcff72a4c499763e54b8300f72
Frederick Johnson <fritzj96@gmail.com>
2022-02-24 10:40:12 -0600

    added: dj_database_url call to get the db config

--- server/core/settings.py
+++ server/core/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_
+    'default': dj_database_url.config()
 }
 
 
