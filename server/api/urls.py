8a78d0b3bfd2c63eb7bc9a58c2f2370913b711f4
Frederick Johnson <fritzj96@gmail.com>
2022-02-28 12:08:50 -0600

    added: vehicle URLS

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,20 @@
-from .views import TemplateCreate, TemplateDetail, TemplateList
+from .views import (
+    TemplateCreate,
+    TemplateDetail,
+    TemplateList,
+    VehicleCreate,
+    VehicleDetail,
+    VehicleList,
+)
 from django.urls import path
 
 urlpatterns = [
-    path("template/", TemplateList.as_view(), name="Template Listd"),
+    # Template Routes
+    path("template/", TemplateList.as_view(), name="Template List"),
     path("template/create/", TemplateCreate.as_view(), name="Template Create"),
     path("template/<int:pk>/", TemplateDetail.as_view(), name="Template Detail"),
+    # Vehicle Routes
+    path("vehicle/", VehicleList.as_view(), name="Vehicle List"),
+    path("vehicle/create/", VehicleCreate.as_view(), name="Vehicle Create"),
+    path("vehicle/<int:pk>/", VehicleDetail.as_view(), name="Vehicle Detail"),
 ]
